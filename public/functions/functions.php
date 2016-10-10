da1bcc4980afcb8de5b1d5f2b84d91734b6c01ce
Norris <im@justnorris.com>
2016-10-10 18:47:14 +0300

    Don’t use post_class() in the background. Invalid styles might be applied

--- public/functions/functions.php
+++ public/functions/functions.php
@@ -38,21 +38,21 @@ function CMP_Instance() {
 
 function cmp_get_class( $class = NULL, $post_id = NULL ) {
 
+	$classes = array();
+
 	if ( $class ) {
 		if ( ! is_array( $class ) ) {
 			$class = preg_split( '#\s+#', $class );
 		}
-		$class = array_map( 'esc_attr', $class );
-	}
-	else {
-		// Ensure that we always coerce class to being an array.
-		$class = array();
+		$classes = array_map( 'esc_attr', $class );
 	}
 
 	// Add Gallery--{{type}} to class
-	$class[] = 'Gallery--' . cmp_get_layout( $post_id );;
+	$classes[] = 'Gallery--' . cmp_get_layout( $post_id );
 
-	return get_post_class( $class );
+	$classes = array_map( 'esc_attr', $classes );
+
+	return array_unique( $classes );
 }
 
 
