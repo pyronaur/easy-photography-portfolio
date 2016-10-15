50b0664514f691a1a0c4e456df2c9a22065bf64e
Norris <im@justnorris.com>
2016-10-15 17:42:45 +0300

    Add apply_filters() and get_post_meta() to cmp_get_option()

--- public/functions/options.php
+++ public/functions/options.php
@@ -12,6 +12,35 @@ if ( ! function_exists( "cmp_get_option" ) ) {
 	 */
 	function cmp_get_option( $option, $default = NULL ) {
 
+
+		/**
+		 * Priority #1: `apply_filters()`
+		 * Allow quick override through WP Filters
+		 *
+		 * Filters have been touched if `apply_filters()` returns anything else than `[undefined]`
+		 * In that case, return that value
+		 */
+		$value = apply_filters( 'cmp_option_' . $option, '[undefined]', $default );
+		if ( $value !== '[undefined]' ) {
+			return $value;
+		}
+
+		/**
+		 * Priority #2: `get_post_meta()`
+		 * Get `$option` from post meta
+		 */
+		if ( in_the_loop() ) {
+			$value = get_post_meta( get_the_ID(), $option, true );
+
+			if ( $value && $value !== $default ) {
+				return $value;
+			}
+		}
+
+		/**
+		 * Priority #3: `cmb2_get_option()`
+		 * Get the option from wordpress options
+		 */
 		$value = cmb2_get_option( 'cmp_options', $option );
 
 		if ( ! $value && $default !== NULL ) {
