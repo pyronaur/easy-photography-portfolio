4572f24fdbfbd9861710ea061b0b466385745fa9
Norris <im@justnorris.com>
2016-10-17 20:00:30 +0300

    Improve \Frontend\Template class

--- Photography_Portfolio/Frontend/Template.php
+++ Photography_Portfolio/Frontend/Template.php
@@ -8,44 +8,22 @@ class Template {
 
 	public static function get( $slug, $name = NULL ) {
 
-
-		$debug_mode    = false;
-		$template      = '';
-		$full_filename = "{$slug}-{$name}.php";
-		$slug_filename = "{$slug}.php";
-
-
-
-		// Get __THEME__/portfolio/slug-name.php
-		if ( $name && ! $debug_mode ) {
-			$template = locate_template( CLM_THEME_PATH . $full_filename );
-		}
-
-		// Get __PLUGIN__/templates/slug-name.php
-		if ( ! $template && $name && file_exists( CLM_PLUGIN_THEME_PATH . $full_filename ) ) {
-			$template = CLM_PLUGIN_THEME_PATH . $full_filename;
-		}
+		$template = false;
 
 
 		/**
-		 *  slug-name.php not found, look for slug.php:
+		 * Try to look for slug-name.php first
 		 */
-
-		// Get __THEME__/portfolio/slug.php
-		if ( ! $template && ! $debug_mode ) {
-			$template = locate_template( CLM_THEME_PATH . $slug_filename );
+		if ( $name ) {
+			$template = self::locate_template( "{$slug}-{$name}.php" );
 		}
 
-		// Get __PLUGIN__/templates/slug.php
-		if ( ! $template && file_exists( CLM_PLUGIN_THEME_PATH . $slug_filename ) ) {
-			$template = CLM_PLUGIN_THEME_PATH . $slug_filename;
+		if ( ! $template ) {
+			$template = self::locate_template( "{$slug}.php" );
 		}
-
-		
 
 		// Allow 3rd party plugins to filter template file from their plugin.
 		$template = apply_filters( 'cmp/template/get', $template, $slug, $name );
-
 
 		/**
 		 * Load Template if template is found
@@ -54,6 +32,25 @@ class Template {
 			load_template( $template, false );
 		}
 
+	}
+
+
+	public static function locate_template( $filename ) {
+
+		$debug_mode = false;
+		$template   = false;
+
+		// Get __THEME__/portfolio/filename.php
+		if ( ! $debug_mode ) {
+			$template = locate_template( CLM_THEME_PATH . $filename );
+		}
+
+		// Get __PLUGIN__/templates/filename.php
+		if ( ! $template && file_exists( CLM_PLUGIN_THEME_PATH . $filename ) ) {
+			$template = CLM_PLUGIN_THEME_PATH . $filename;
+		}
+
+		return $template;
 	}
 
 }
