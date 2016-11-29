60e9c3c6c57c8df73c050e96714570aa722ad829
Norris <im@justnorris.com>
2016-11-29 12:46:15 +0200

    Remove function exists check. pp_get_option is not overrideable.

--- public/functions/options.php
+++ public/functions/options.php
@@ -1,57 +1,55 @@
 <?php
 
 
-if ( ! function_exists( "pp_get_option" ) ) {
+/**
+ * Easy way to get option values
+ *
+ * @param      $option
+ * @param null $default
+ *
+ * @return mixed|null
+ */
+function pp_get_option( $option, $default = NULL ) {
+
+
 	/**
-	 * Easy way to get option values
+	 * Priority #1: `apply_filters()`
+	 * Allow quick override through WP Filters
 	 *
-	 * @param      $option
-	 * @param null $default
-	 *
-	 * @return mixed|null
+	 * Filters have been touched if `apply_filters()` returns anything else than `[undefined]`
+	 * In that case, return that value
 	 */
-	function pp_get_option( $option, $default = NULL ) {
+	$value = apply_filters( 'pp_option_' . $option, '[undefined]', $default );
+	if ( $value !== '[undefined]' ) {
+		return $value;
+	}
 
+	/**
+	 * Priority #2: `get_post_meta()`
+	 * Get `$option` from post meta
+	 */
+	if ( in_the_loop() ) {
+		$value = get_post_meta( get_the_ID(), $option, true );
 
-		/**
-		 * Priority #1: `apply_filters()`
-		 * Allow quick override through WP Filters
-		 *
-		 * Filters have been touched if `apply_filters()` returns anything else than `[undefined]`
-		 * In that case, return that value
-		 */
-		$value = apply_filters( 'pp_option_' . $option, '[undefined]', $default );
-		if ( $value !== '[undefined]' ) {
+		if ( $value && $value !== $default ) {
 			return $value;
 		}
-
-		/**
-		 * Priority #2: `get_post_meta()`
-		 * Get `$option` from post meta
-		 */
-		if ( in_the_loop() ) {
-			$value = get_post_meta( get_the_ID(), $option, true );
-
-			if ( $value && $value !== $default ) {
-				return $value;
-			}
-		}
-
-		/**
-		 * Priority #3: `cmb2_get_option()`
-		 * Get the option from wordpress options
-		 */
-		$value = cmb2_get_option( 'pp_options', $option );
-
-		if ( ! $value && $default !== NULL ) {
-			return $default;
-		}
-
-
-		return $value;
-
-
 	}
+
+	/**
+	 * Priority #3: `cmb2_get_option()`
+	 * Get the option from wordpress options
+	 */
+	$value = cmb2_get_option( 'pp_options', $option );
+
+	if ( ! $value && $default !== NULL ) {
+		return $default;
+	}
+
+
+	return $value;
+
+
 }
 
 
