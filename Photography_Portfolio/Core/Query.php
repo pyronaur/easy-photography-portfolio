de9ca352f01d8cfedd6a4889c833db22d43b8244
Norris <im@justnorris.com>
2016-12-05 18:46:05 +0200

    Clean up \Core\Query and store unaltered wp_query before touching it

--- Photography_Portfolio/Core/Query.php
+++ Photography_Portfolio/Core/Query.php
@@ -6,6 +6,7 @@ namespace Photography_Portfolio\Core;
 
 class Query {
 
+	public    $original_query;
 	protected $is_archive;
 	protected $is_single;
 	protected $is_category;
@@ -20,30 +21,16 @@ class Query {
 	}
 
 
-	public function is_portfolio() {
-
-		return ( $this->is_archive || $this->is_single || $this->is_category );
-	}
-
-
-	public function is_archive() {
-
-		return $this->is_archive;
-	}
-
-
-	public function is_category() {
-
-		return $this->is_category;
-	}
-
-
 	public function set_variables( \WP_Query $query ) {
 
 		if ( ! $query->is_main_query() ) {
 			return;
 		}
-		
+
+		// Store the original query
+		$this->original_query = clone $query;
+
+		// Alter the $wp_query
 		$this->set_is_archive( $query );
 		$this->set_is_category( $query );
 		$this->set_is_single( $query );
@@ -66,13 +53,14 @@ class Query {
 		 *  Check if current Page is supposed to be an Archive
 		 *  Modify the WP_Query if so:
 		 */
-		if ( ( $this->is_portfolio_front_page_active() && $this->is_portfolio_front_page( $query ) ) || $this->is_portfolio_page( $query ) ) {
+		if ( $this->front_page_is_portfolio() && $this->is_portfolio_page() ) {
 
 
 			// modify query_vars:
 			$query->set( 'post_type', 'pp_post' );  // override 'post_type'
 			$query->set( 'pagename', NULL );  // override 'pagename'
 			$query->set( 'page_id', '' );  // override 'pagename'
+
 			$query->set( 'posts_per_page', - 1 );
 
 
@@ -101,41 +89,30 @@ class Query {
 	protected function set_is_single( \WP_Query $query ) {
 
 		$is_pp_post      = ( $query->get( 'post_type' ) === 'pp_post' );
-		$this->is_single = ( $is_pp_post
-		                     && (
-			                     $query->is_single() || $this->is_portfolio_front_page( $query )
-		                     )
+		$this->is_single = ( $is_pp_post &&
+		                     ( $query->is_single() || $query->is_singular( 'pp_post' ) )
 		);
 
 		return $this->is_single;
 	}
 
 
-	public function is_portfolio_front_page_active() {
+	public function front_page_is_portfolio() {
 
-		return ( pp_get_option( 'portfolio_page', false ) == get_option( 'page_on_front' ) );
+		return ( get_option( 'show_on_front' ) == 'page' && pp_get_option( 'portfolio_page', false ) == get_option( 'page_on_front' ) );
 	}
 
 
-	public function is_portfolio_front_page( \WP_Query $query ) {
+	public function is_portfolio_page() {
 
-		if ( ! is_a( $query, 'WP_Query' ) ) {
-			return false;
+		$id = (int) get_queried_object_id();
+
+		if ( ! $id ) {
+			$id = (int) $this->original_query->get( 'page_id' );
 		}
 
-		return ( get_option( 'show_on_front' ) == 'page'
-		         && get_option( 'page_on_front' )
-		         && $query->get( 'page_id' ) == get_option( 'page_on_front' )
-		);
+		return ( $id > 0 && $id === (int) pp_get_option( 'portfolio_page', 0 ) );
 
-	}
-
-
-	public function is_portfolio_page( \WP_Query $query ) {
-
-		$qID = $query->get_queried_object_id();
-
-		return ( $qID > 0 && ( (int) $qID === (int) pp_get_option( 'portfolio_page', false ) ) );
 	}
 
 
@@ -143,5 +120,24 @@ class Query {
 
 		return $this->is_single;
 	}
+
+
+	public function is_portfolio() {
+
+		return ( $this->is_archive || $this->is_single || $this->is_category );
+	}
+
+
+	public function is_archive() {
+
+		return $this->is_archive;
+	}
+
+
+	public function is_category() {
+
+		return $this->is_category;
+	}
+
 
 }
