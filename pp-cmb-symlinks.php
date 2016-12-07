bcf05a8ceba558c5b02648793f382cbf3330d4e0
Norris <im@justnorris.com>
2016-12-07 11:23:14 +0200

    Prefix symlinks function

--- pp-cmb-symlinks.php
+++ pp-cmb-symlinks.php
@@ -3,9 +3,9 @@
 /**
  * Support Symlinks in Development environmnent
  */
-function update_cmb2_meta_box_url( $url ) {
+function pp_update_cmb2_meta_box_url( $url ) {
 
 	return plugin_dir_url( __FILE__ ) . '/vendor/webdevstudios/cmb2';
 }
 
-add_filter( 'cmb2_meta_box_url', 'update_cmb2_meta_box_url' );
+add_filter( 'cmb2_meta_box_url', 'pp_update_cmb2_meta_box_url' );
