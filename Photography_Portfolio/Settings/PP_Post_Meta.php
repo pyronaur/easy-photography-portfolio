03b525425b10fdf37d522ed85b13af39d445f250
Norris <im@justnorris.com>
2016-12-07 15:00:44 +0200

    Basic working `pp_subtitle`

--- Photography_Portfolio/Settings/PP_Post_Meta.php
+++ Photography_Portfolio/Settings/PP_Post_Meta.php
@@ -12,27 +12,75 @@ class PP_Post_Meta {
 	 */
 	public function __construct() {
 
+		add_action( 'cmb2_init', [ $this, 'before_content' ] );
 		add_action( 'cmb2_init', [ $this, 'after_content' ] );
+
 
 		/**
 		 * @TODO: Implement this in a pretty way, this needs CSS and JavaScript
 		 */
-		// add_action( 'edit_form_after_title', [ $this, 'move_metabox_after_post_title' ] );
+		add_action( 'edit_form_after_title', [ $this, 'move_metabox_to_after_title' ] );
 
 
 	}
 
-//	/**
-//	 * Display checkbox metabox below title field
-//	 * @link https://github.com/WordPress/WordPress/blob/56d6682461be82da1a3bafc454dad2c9da451a38/wp-admin/edit-form-advanced.php#L517-L523
-//	 */
-//	public function move_metabox_after_post_title() {
-//
-//		$cmb = cmb2_get_metabox( 'pp_post_meta' );
-//		if ( in_array( get_post_type(), $cmb->prop( 'object_types' ), 1 ) ) {
-//			$cmb->show_form();
-//		}
-//	}
+
+	/**
+	 * Display checkbox metabox below title field
+	 * @link https://github.com/WordPress/WordPress/blob/56d6682461be82da1a3bafc454dad2c9da451a38/wp-admin/edit-form-advanced.php#L517-L523
+	 */
+	public function move_metabox_to_after_title() {
+
+		$cmb = cmb2_get_metabox( 'pp_post_before_meta' );
+		if ( in_array( get_post_type(), $cmb->prop( 'object_types' ), 1 ) ) {
+			$this->wrapper_open();
+			$cmb->show_form();
+			$this->wrapper_close();
+		}
+	}
+
+
+	public function wrapper_open() {
+
+		echo '<div class="postbox" style="margin-top: 3em;"><div class="inside">';
+	}
+
+
+	public function wrapper_close() {
+
+		echo '</div></div>';
+	}
+
+
+	public function before_content() {
+
+		$cmb = new_cmb2_box(
+			array(
+				'id'           => 'pp_post_before_meta',
+				'title'        => 'Portfolio',
+				'object_types' => [ 'pp_post' ],
+				'show_names'   => true,
+				'context'      => 'normal',
+				'priority'     => 'high',
+			)
+		);
+
+		$cmb->add_field(
+			array(
+
+				'desc'  => '',
+				'name'  => 'Subtitle',
+				'title' => 'Portfolio',
+				'id'    => 'pp_subtitle',
+				'type'  => 'text',
+			)
+		);
+
+
+		// Allow metabox extension
+		do_action( 'pp/meta/before_content', $cmb );
+	}
+
 
 	public function after_content() {
 
@@ -40,7 +88,7 @@ class PP_Post_Meta {
 			array(
 				'id'           => 'pp_post_after_meta',
 				'title'        => __( 'Photography Portfolio', 'pp-plugin' ),
-				'object_types' => array( 'pp_post' ),
+				'object_types' => [ 'pp_post' ],
 				'context'      => 'normal',
 				'priority'     => 'high',
 			)
@@ -52,23 +100,20 @@ class PP_Post_Meta {
 				'desc'         => '',
 				'id'           => 'pp_gallery',
 				'type'         => 'file_list',
-				'preview_size' => array( 125, 125, true ), // Default: array( 50, 50 )
+				'preview_size' => [ 125, 125, true ], // Default: array( 50, 50 )
 				// Optional, override default text strings
-				'text'         => array(
+				'text'         => [
 					'add_upload_files_text' => esc_html__( 'Add Images', 'pp-plugin' ),
 					'remove_image_text'     => esc_html__( 'Remove Image', 'pp-plugin' ),
 					'file_text'             => esc_html__( 'File:', 'pp-plugin' ),
 					'file_download_text'    => esc_html__( 'Download', 'pp-plugin' ),
 					'remove_text'           => esc_html__( 'Remove', 'pp-plugin' ),
-				),
+				],
 			)
 		);
 
 
-		/*
-		 * Allow metabox extension
-		 */
+		// Allow metabox extension
 		do_action( 'pp/meta/after_content', $cmb );
-
 	}
 }
