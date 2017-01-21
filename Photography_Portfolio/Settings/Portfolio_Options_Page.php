233be9765c889ebc2c342137b3eb1d1b269f3b21
Norris <im@justnorris.com>
2017-01-21 09:10:51 +0200

    Show “Layout Settings” only if they have options to choose from

--- Photography_Portfolio/Settings/Portfolio_Options_Page.php
+++ Photography_Portfolio/Settings/Portfolio_Options_Page.php
@@ -25,78 +25,114 @@ class Portfolio_Options_Page implements Options_Page_Settings_Interface {
 
 	public function set_fields( \CMB2 $cmb ) {
 
-		$this->pages();
+
+		$PP = phort_instance();
+
+		$archive_layouts     = $PP->layouts->available_layouts( 'archive' );
+		$single_layouts      = $PP->layouts->available_layouts( 'single' );
+		$has_layout_settings = ( count( $archive_layouts ) > 1 || count( $single_layouts ) > 1 );
+
+
+		/**
+		 *
+		 * ======== Core Settings
+		 *
+		 */
 		$cmb->add_field(
-			array(
+			[
 				'id'               => "portfolio_page",
 				'name'             => esc_html__( 'Portfolio Page', 'phort-plugin' ),
 				'type'             => 'select',
 				'show_option_none' => true,
 				'options'          => $this->pages(),
 
-			)
+			]
 		);
 
 
-		// Set our CMB2 fields
 		$cmb->add_field(
-			array(
-				'name'    => esc_html__( 'Portfolio Archive Layout', 'phort-plugin' ),
-				'id'      => 'portfolio_layout',
-				'type'    => 'select',
-				'options' => phort_instance()->layouts->available_layouts( 'archive' ),
-
-			)
-		);
-
-		$cmb->add_field(
-			array(
+			[
 				'name'    => esc_html__( 'Show Titles & Descriptions in Archives', 'phort-plugin' ),
 				'id'      => 'archive_enable_description',
 				'type'    => 'checkbox',
 				'default' => false,
 
-			)
-		);
-
-		$cmb->add_field(
-			array(
-				'name'    => esc_html__( 'Single Portfolio Layout', 'phort-plugin' ),
-				'id'      => 'single_portfolio_layout',
-				'type'    => 'select',
-				'options' => phort_instance()->layouts->available_layouts( 'single' ),
-
-			)
+			]
 		);
 
 
 		$cmb->add_field(
-			array(
+			[
 				'name'    => esc_html__( 'Enable Portfolio Subtitles', 'phort-plugin' ),
+				'desc'    => esc_html__( 'This will add a subtitle field to your Portfolio Posts', 'phort-plugin' ),
 				'id'      => 'portfolio_enable_subtitle',
 				'type'    => 'checkbox',
 				'default' => true,
 
-			)
+			]
 		);
 
-
 		$cmb->add_field(
-			array(
+			[
 				'id'       => "portfolio_show_image_count",
 				'name'     => esc_html__( 'Show image count in subtitles', 'phort-plugin' ),
-				'required' => array( 'portfolio_enable_subtitle', '=', '1' ),
+				'desc'     => esc_html__( '"Portfolio Subtitles" must be enabled for this setting to work.', 'phort-plugin' ),
+				'required' => [ 'portfolio_enable_subtitle', '=', '1' ],
 
 				'type'    => 'select',
-				'options' => array(
+				'options' => [
 					'disable'      => "Disable",
 					'only_missing' => "Only Enable when a sub-title is empty",
 					'always'       => 'Always Enable (override sub-titles)',
-				),
+				],
 				'default' => 'disable',
 
-			)
+			]
 		);
+
+
+		/**
+		 *
+		 * ======== Layout Settings
+		 *
+		 */
+
+		if ( $has_layout_settings ) {
+			$cmb->add_field(
+				[
+					'name' => 'Layout Settings',
+					'type' => 'title',
+					'id'   => 'layout_settings_title',
+				]
+			);
+		}
+
+		/**
+		 * Hide "Single Portfolio Layout" Option, if there is only 1 layout available
+		 */
+		if ( count( $single_layouts ) > 1 ) {
+			$cmb->add_field(
+				[
+					'name'    => esc_html__( 'Single Portfolio Layout', 'phort-plugin' ),
+					'id'      => 'single_portfolio_layout',
+					'type'    => 'select',
+					'options' => $single_layouts,
+
+				]
+			);
+		}
+
+		if ( count( $archive_layouts ) > 1 ) {
+			$cmb->add_field(
+				[
+					'name'    => esc_html__( 'Portfolio Archive Layout', 'phort-plugin' ),
+					'id'      => 'portfolio_layout',
+					'type'    => 'select',
+					'options' => $archive_layouts,
+
+				]
+			);
+		}
 
 
 		/**
@@ -104,7 +140,7 @@ class Portfolio_Options_Page implements Options_Page_Settings_Interface {
 		 */
 		if ( ! phort_has_theme_support() ) {
 			$cmb->add_field(
-				array(
+				[
 					'id'      => "wrapper_class",
 					'name'    => esc_html__( 'Wrapper CSS Classes', 'phort-plugin' ),
 					'desc'    => esc_html__(
@@ -115,7 +151,7 @@ class Portfolio_Options_Page implements Options_Page_Settings_Interface {
 					'type'    => 'text',
 					'default' => '',
 
-				)
+				]
 			);
 		}
 
@@ -125,13 +161,13 @@ class Portfolio_Options_Page implements Options_Page_Settings_Interface {
 
 	public function pages() {
 
-		$args = array(
+		$args = [
 			'posts_per_page' => - 1,
 			'post_type'      => 'page',
 			'post_status'    => 'publish',
-		);
+		];
 
-		$pages = array();
+		$pages = [];
 
 		foreach ( get_posts( $args ) as $post ) {
 			$pages[ (int) $post->ID ] = esc_html( $post->post_title );
