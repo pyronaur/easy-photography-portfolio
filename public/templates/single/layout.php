1e528148c7ac22ffb8652fd1b2a13d4227b7903c
Norris <im@justnorris.com>
2017-02-20 12:52:40 +0200

    Add actions `phort/gallery/loop/{start|end}`

--- public/templates/single/layout.php
+++ public/templates/single/layout.php
@@ -20,15 +20,17 @@
 	?>
 	<div <?php phort_class( 'PP_Gallery' ) ?>>
 
+		<?php do_action( 'phort/gallery/loop/start' ) ?>
+
 		<?php
 		if ( phort_gallery_has_items() ):
 			while ( phort_gallery_has_items() ): phort_gallery_setup_item();
-
 				phort_get_template( 'gallery/loop-item' );
-
 			endwhile;
 		endif;
 		?>
+
+		<?php do_action( 'phort/gallery/loop/end' ) ?>
 
 	</div> <!-- .PP_Gallery -->
 
