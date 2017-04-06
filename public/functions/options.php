dda059ba5f96b253137432a2c07063c6ffe2a919
Norris <im@justnorris.com>
2017-04-06 18:09:11 +0300

    `phort_get_option` now automatically fetches the default when needed
    Deprecate the $default variable

--- public/functions/options.php
+++ public/functions/options.php
@@ -5,12 +5,14 @@
  * Easy way to get option values
  *
  * @param      $option
- * @param null $default
+ * @param null $default @deprecated
  *
  * @return mixed|null
  */
-function phort_get_option( $option, $default = NULL ) {
+function phort_get_option( $option, $deprecated = '' ) {
 
+	$default    = '';
+	$_undefined = '[undefined]';
 
 	/**
 	 * Priority #1: `apply_filters()`
@@ -19,10 +21,20 @@ function phort_get_option( $option, $default = NULL ) {
 	 * Filters have been touched if `apply_filters()` returns anything else than `[undefined]`
 	 * In that case, return that value
 	 */
-	$value = apply_filters( 'phort_option_' . $option, '[undefined]', $default );
-	if ( $value !== '[undefined]' ) {
+	$value = apply_filters( 'phort_option_' . $option, $_undefined );
+	if ( $value !== $_undefined ) {
 		return $value;
 	}
+
+
+	/**
+	 * Setup the default value
+	 */
+	$setting = phort_instance()->settings->get( $option );
+	if ( isset( $setting['default'] ) ) {
+		$default = $setting['default'];
+	}
+
 
 	/**
 	 * Priority #2: `get_post_meta()`
@@ -38,21 +50,18 @@ function phort_get_option( $option, $default = NULL ) {
 
 	/**
 	 * Priority #3: `cmb2_get_option()`
-	 * Get the option from wordpress options
+	 * Get the global option from wordpress options
 	 */
 	$options = get_option( 'phort_options' );
 	if ( isset( $options[ $option ] ) ) {
 		$value = $options[ $option ];
 	}
 
-	if ( ! $value && $default !== NULL ) {
+	if ( $value === $_undefined ) {
 		return $default;
 	}
 
-
 	return $value;
-
-
 }
 
 
