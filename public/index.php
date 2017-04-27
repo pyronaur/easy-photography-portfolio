741337e555b3f58274f574d4c44eef7122b99ec0
Norris <im@justnorris.com>
2017-04-27 19:19:13 +0300

    Improve inline documentation

--- public/index.php
+++ public/index.php
@@ -8,14 +8,14 @@
  * ---
  *
  * + public/assets/
- *      Styles, scripts, icons, imagees
+ *      Styles, scripts, icons, images
  *
  * + public/functions/
  *      Template functions that you can use in themes, instead of having to rely on classes and namespaces.
  *
  * + public/templates/
  *      That magical direcotry where the plugin looks for layouts.
- *      Copy this to your /theme/portfolio/ and override what you must.
+ *      Copy this to your /your-theme-name/photography-portfolio/ and override what you must.
  *
  *
  */
