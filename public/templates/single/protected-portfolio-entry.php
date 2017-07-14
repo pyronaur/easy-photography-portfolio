3a7e064619ccaddb97e0eae4f2d8a3b0a5aa340e
Norris <im@justnorris.com>
2017-07-14 13:20:55 +0300

    Add i18n

--- public/templates/single/protected-portfolio-entry.php
+++ public/templates/single/protected-portfolio-entry.php
@@ -1,6 +1,6 @@
 <div <?php phort_class( 'PP_Protected' ) ?>>
 
-	<h1>Protected Gallery</h1>
+	<h1><?php esc_html_e('Protected Gallery','photography-portfolio'); ?></h1>
 	<?php echo get_the_password_form(); ?>
 
 </div>
