557f83ddbeb67368fc2c653da50d4695bbcc7f37
Norris <im@justnorris.com>
2017-07-31 18:20:56 +0300

    Only show thumbnails option for lightGallery gallery

--- sources/script/admin/phort-admin.js
+++ sources/script/admin/phort-admin.js
@@ -1,6 +1,22 @@
-(function () {
+(function ( $ ) {
     'use strict'
 
-    // do things & stuff
 
-})()
+    var show_lightgallery_thumbnails_option = function () {
+
+        var selected = $( '#popup_gallery' ).val()
+        var $row     = $( '#lg_thumbnails' ).closest( '.cmb-row' )
+
+        if ( selected === 'lightgallery' ) {
+            $row.show()
+        } else {
+            $row.hide()
+        }
+
+    }
+
+
+    $( document ).ready( show_lightgallery_thumbnails_option )
+    $( document ).on( 'change', '#popup_gallery', show_lightgallery_thumbnails_option )
+
+})( jQuery )
