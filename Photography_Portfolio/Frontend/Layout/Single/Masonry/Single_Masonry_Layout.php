c29c0708e41fb43777787bda0333d885c4d363e2
Norris <im@justnorris.com>
2017-08-07 20:44:14 +0300

    Square brackedet arrays

--- Photography_Portfolio/Frontend/Layout/Single/Masonry/Single_Masonry_Layout.php
+++ Photography_Portfolio/Frontend/Layout/Single/Masonry/Single_Masonry_Layout.php
@@ -7,21 +7,18 @@ namespace Photography_Portfolio\Frontend\Layout\Single\Masonry;
 use Photography_Portfolio\Frontend\Layout\Single\Single_Portfolio_Layout;
 
 class Single_Masonry_Layout extends Single_Portfolio_Layout {
-	
-	public $attached_sizes = array(
+
+	public $attached_sizes = [
 
 		'thumb' => 'large',
 		'full'  => 'full',
 
-	);
-
-	public $appended_classes  = [
-		'PP_Gallery' => 'PP_Masonry',
-	    'PP_Gallery__item' => 'PP_Masonry__item'
 	];
 
-
-
+	public $appended_classes = [
+		'PP_Gallery'       => 'PP_Masonry',
+		'PP_Gallery__item' => 'PP_Masonry__item',
+	];
 
 
 }
