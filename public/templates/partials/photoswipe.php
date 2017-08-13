682ded15cc31e87981faada6c90c1389f184faf7
Norris <im@justnorris.com>
2017-08-13 16:13:43 +0300

    Some themes style buttons globally. Use link elements instead.

--- public/templates/partials/photoswipe.php
+++ public/templates/partials/photoswipe.php
@@ -26,13 +26,13 @@
 
 				<div class="pswp__counter"></div>
 
-				<button class="pswp__button pswp__button--close" title="<?php esc_html_e( 'Close (Esc)', 'photography-portfolio' ); ?>"></button>
+				<a class="pswp__button pswp__button--close" title="<?php esc_html_e( 'Close (Esc)', 'photography-portfolio' ); ?>"></a>
 
-				<button class="pswp__button pswp__button--share" title="<?php esc_html_e( 'Share', 'photography-portfolio' ); ?>"></button>
+				<a class="pswp__button pswp__button--share" title="<?php esc_html_e( 'Share', 'photography-portfolio' ); ?>"></a>
 
-				<button class="pswp__button pswp__button--fs" title="<?php esc_html_e( 'Toggle fullscreen', 'photography-portfolio' ); ?>"></button>
+				<a class="pswp__button pswp__button--fs" title="<?php esc_html_e( 'Toggle fullscreen', 'photography-portfolio' ); ?>"></a>
 
-				<button class="pswp__button pswp__button--zoom" title="<?php esc_html_e( 'Zoom in/out', 'photography-portfolio' ); ?>"></button>
+				<a class="pswp__button pswp__button--zoom" title="<?php esc_html_e( 'Zoom in/out', 'photography-portfolio' ); ?>"></a>
 
 				<!-- Preloader demo http://codepen.io/dimsemenov/pen/yyBWoR -->
 				<!-- element will get class pswp__preloader--active when preloader is running -->
@@ -49,11 +49,11 @@
 				<div class="pswp__share-tooltip"></div>
 			</div>
 
-			<button class="pswp__button pswp__button--arrow--left" title="<?php esc_html_e( 'Previous (arrow left)', 'photography-portfolio' ); ?>">
-			</button>
+			<a class="pswp__button pswp__button--arrow--left" title="<?php esc_html_e( 'Previous (arrow left)', 'photography-portfolio' ); ?>">
+			</a>
 
-			<button class="pswp__button pswp__button--arrow--right" title="<?php esc_html_e( 'Next (arrow right)', 'photography-portfolio' ); ?>">
-			</button>
+			<a class="pswp__button pswp__button--arrow--right" title="<?php esc_html_e( 'Next (arrow right)', 'photography-portfolio' ); ?>">
+			</a>
 
 			<div class="pswp__caption">
 				<div class="pswp__caption__center"></div>
