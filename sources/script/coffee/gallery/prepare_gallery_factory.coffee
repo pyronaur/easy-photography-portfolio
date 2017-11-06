pyronaur/easy-photography-portfolio

commit 1445db5185812e8f344d5635e8e31968f9a46544

Refactor gallery adapters
Add comments, clean up files

sources/script/coffee/gallery/prepare_gallery_factory.coffee
@@ -1,0 +1,40 @@
+#
+# This file is going to return a [Gallery Factory] instance
+# Easy Photography Portfolio is using that to open/close/destroy galleries
+#
+# [Gallery Factory] relies on a [Adapter]
+# Instead of relying directly on a dependency, Gallery Factory relies on a Adapter that can be modified
+# A Adapter is an adapter for a Popup-Gallery plugin, such as LightGallery or PhotoSwipe
+#
+# So when a gallery is opened, this is probably how it's going to look:
+# [Gallery Factory] asks [Adapter] to find and open a gallery with [any LightBox Library]
+#
+
+# -------------------------
+# Gallery Adapter:
+# -------------------------
+setup_driver = ( driver_name = 'lightgallery' ) ->
+	if driver_name is 'lightgallery'
+		adapter = require( './adapters/lightgallery' )
+
+	if driver_name is 'photoswipe'
+		adapter = require( './adapters/photoswipe' )
+
+	return Hooks.applyFilters( 'phort.gallery.driver', adapter )
+
+# -------------------------
+# Gallery Factory:
+# -------------------------
+# The gallery factory is what we're interacting with to open/close a gallery
+setup_factory = ->
+	factory = require( './gallery_factory' )
+	return Hooks.applyFilters( 'phort.gallery.factory', factory )
+
+#
+# Return a factory instance
+#
+module.exports = ->
+	gallery_driver = setup_driver( window.__phort.popup_gallery )
+	gallery_factory = setup_factory( )
+
+	return gallery_factory( gallery_driver )

sources/script/coffee/gallery/start.coffee
@@ -3,32 +3,7 @@
 ###
 $ = require( 'jQuery' )
 Hooks = require( 'wp_hooks' )
-
-# Gallery Driver:
-# The driver is responsible for adapting the popup gallery to Easy Photography Portfolio
-setup_driver = ( driver_name = 'lightgallery' ) ->
-	if driver_name is 'lightgallery'
-		Driver = require( './drivers/lightgallery' )
-
-	if driver_name is 'photoswipe'
-		Driver = require( './drivers/photoswipe' )
-
-	return Hooks.applyFilters( 'phort.gallery.driver', Driver )
-
-# Gallery Factory:
-# The gallery factory is what we're interacting with to open/close a gallery
-setup_factory = ->
-	factory = require( './gallery_factory' )
-	return Hooks.applyFilters( 'phort.gallery.factory', factory )
-
-
-# --------------------------
-# Initialize:
-#
-gallery_driver = setup_driver( window.__phort.popup_gallery )
-gallery_factory = setup_factory()
-Gallery = gallery_factory( gallery_driver )
-
+Gallery = require( './prepare_gallery_factory' )
 
 # Click
 $( document ).on 'click', '.PP_Gallery__item', ( e ) ->

commit 5f095d2465d91fecc9c054a15351a30fe66fbbc9

Fix bug introduced in 1445db5185812e8f344d5635e8e31968f9a46544

sources/script/coffee/gallery/prepare_gallery_factory.coffee
@@ -1,3 +1,5 @@
+Hooks = require("wp_hooks")
+
 #
 # This file is going to return a [Gallery Factory] instance
 # Easy Photography Portfolio is using that to open/close/destroy galleries
@@ -33,8 +35,8 @@ setup_factory = ->
 #
 # Return a factory instance
 #
-module.exports = ->
-	gallery_driver = setup_driver( window.__phort.popup_gallery )
-	gallery_factory = setup_factory( )
 
-	return gallery_factory( gallery_driver )
+gallery_driver = setup_driver( window.__phort.popup_gallery )
+gallery_factory = setup_factory( )
+
+module.exports = gallery_factory( gallery_driver )