a57ad5e0980b583320d1e964945fa30fdac773d4
Norris <im@justnorris.com>
2017-12-04 16:48:10 +0200

    Move `PP_Gallery` from `single/portfolio-entry` to `gallery/gallery-loop`

--- public/functions/hooks.php
+++ public/functions/hooks.php
@@ -17,3 +17,25 @@ function phort_inline_data_image_size() {
 }
 
 add_action( 'phort/archive/entry/attributes', 'phort_inline_data_image_size' );
+
+
+/**
+ * @since 1.4.0
+ * New gallery loop action hook:
+ * `gallery-loop` instead of `gallery/loop`
+ */
+add_action(
+	'phort/gallery-loop/start',
+	function () {
+
+		do_action_deprecated( 'phort/gallery/loop/start', [], '1.4.0' );
+	}
+);
+
+add_action(
+	'phort/gallery-loop/end',
+	function () {
+
+		do_action_deprecated( 'phort/gallery/loop/end', [], '1.4.0' );
+	}
+);
