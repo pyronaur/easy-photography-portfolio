fa01f703ee79dfabc5bf6c04458ab83227d6d488
Norris <im@justnorris.com>
2017-12-21 12:45:54 +0200

    Refactor theme defaults

--- compatibility/unsupported-theme-defaults.php
+++ compatibility/unsupported-theme-defaults.php
@@ -4,17 +4,12 @@
 /**
  * Add some default styling in themes that don't natively support EPP
  */
-add_filter( 'phort_get_class', 'phort_add_styled_wrapper_class' );
-function phort_add_styled_wrapper_class( $classes ) {
+add_action( 'init', 'phort_maybe_add_default_container_styles', 100 );
+function phort_maybe_add_default_container_styles() {
 
 	// Only add the class if the theme doesn't support the plugin
-	if ( phort_has_theme_support() ) {
-		return $classes;
+	if ( ! phort_has_theme_support() ) {
+		phort_attach_class( 'PP_Wrapper', 'PP_Wrapper--default' );
 	}
 
-	if ( in_array( 'PP_Wrapper', $classes ) ) {
-		$classes[] = 'PP_Wrapper--default';
-	}
-
-	return $classes;
 }
