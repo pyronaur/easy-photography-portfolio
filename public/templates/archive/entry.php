dd2505b5f2d96fb75f8161cf5213baffe8469ee4
Norris <im@justnorris.com>
2017-12-21 15:17:20 +0200

    Remove archive/view-entry-link

--- public/templates/archive/entry.php
+++ public/templates/archive/entry.php
@@ -30,6 +30,12 @@
 
 	</div> <!-- .PP_Entry__header -->
 
-	<?php phort_get_template( 'archive/view-entry-link' ) ?>
+	<a class="PP_Entry__more" href="<?php the_permalink(); ?>">
+	<span class="PP_Entry__inner">
+		<span class="PP_Entry__view">
+			<?php esc_html_e( 'View Gallery', 'photography-portfolio' ) ?>
+		</span>
+	</span>
+	</a> <!-- .PP_Entry__more -->
 
 </div>
