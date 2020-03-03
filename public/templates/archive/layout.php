cd0f0984f37c3291b54106814908b66ea8b5309b
DitgerDW <ditger.de.wolf@hotmail.com>
2020-03-03 11:12:37 +0100

    Added a counter to check if there are categories

--- public/templates/archive/layout.php
+++ public/templates/archive/layout.php
@@ -10,11 +10,17 @@
 
 <div <?php phort_class( 'PP_Archive_Filter' ); ?> >
     <div <?php phort_class( 'PP_Archive_Filter_Categories' ); ?> >
-        <span> <?php esc_html("Categories:") ?></span>
-        <?php
-         foreach  (get_terms( 'phort_post_category' ) as $taxonomy) {
-             echo '<a href="' . esc_url(get_category_link($taxonomy->term_id)) . '">' . $terms[ (int) $taxonomy->term_id ] = esc_html( $taxonomy->name ) . ' ' . '</a>';
-         }
+	    <?php
+
+        if (count(get_terms( 'phort_post_category' )) !== 0 ) {
+	        echo "<span>". esc_html("Categories: ") ."</span>";
+
+	        foreach  (get_terms( 'phort_post_category' ) as $taxonomy) {
+		        echo '<a href="' . esc_url(get_category_link($taxonomy->term_id)) . '">' . $terms[ (int) $taxonomy->term_id ] = esc_html( $taxonomy->name ) . ' ' . '</a>';
+	        }
+        } else {
+	        echo "No categories found or is empty!";
+        }
          ?>
     </div>
 </div>
