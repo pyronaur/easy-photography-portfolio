b199a743e18658ca9808492ca739f3ef282218df
DitgerDW <ditger.de.wolf@hotmail.com>
2020-03-06 15:46:47 +0100

    Updated foreach to complete consistency + bug fix that prevented links from working

--- public/templates/archive/layout.php
+++ public/templates/archive/layout.php
@@ -17,11 +17,11 @@
                         <?php esc_html_e( 'Categories:', 'photography-portfolio' ); ?>
                      </span>
 
-			         <?php
-				         foreach  ( get_terms( 'phort_post_category' ) as $taxonomy ) {
-					         echo '<a href="' . esc_url( get_category_link( $taxonomy->term_id ) ) . '">' . esc_html( $taxonomy->name ) . ' ' . '</a>';
-				         }
-			         ?>
+			         <?php foreach  ( get_terms( 'phort_post_category' ) as $taxonomy ): ?>
+                         <a href="<?php echo esc_url( get_category_link( $taxonomy->term_id ) ) ?>">
+                             <?php esc_html_e( $taxonomy->name, 'photography-portfolio' ) ?>
+                         </a>
+			         <?php endforeach; ?>
 		         <?php endif; ?>
     </div>
 </div>
